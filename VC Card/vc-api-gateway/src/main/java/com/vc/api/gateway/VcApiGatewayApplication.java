4597d6b734e7d5a2c4af46068b9b88748b672183
Pradeeb <valanpradeep6@gmail.com>
2025-08-08 10:21:11 +0530

    API gate way set up

---
+++ VC Card/vc-api-gateway/src/main/java/com/vc/api/gateway/VcApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.vc.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcApiGatewayApplication.class, args);
+	}
+
+}
---
+++ VC Card/vc-api-gateway/src/test/java/com/vc/api/gateway/VcApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vc.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
